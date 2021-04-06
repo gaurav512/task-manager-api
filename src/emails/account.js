ff0e6bb7027d679222004e623f04d2f628b9a49b
Gaurav <itsgaurav01@gmail.com>
2021-04-06 19:49:52 +0530

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_PASSWORD
+    }
+})
+
+const sendWelcomeMail = (email, name) => {
+    transporter.sendMail({
+        from: "Task-Manager API <grv.devmail@gmail.com>",
+        to: email,
+        subject: "Welcome to TaskManager!",
+        text: `Welcome to the app, ${name}. I hope we can serve you in the best way possible. If you have any queries about our services, feel free to reply to this email.`
+    })
+}
+
+const sendLeavingMail = (email, name) => {
+    transporter.sendMail({
+        from: "Task-Manager API <grv.devmail@gmail.com>",
+        to: email,
+        subject: "We are sorry to see you go!",
+        text: `Goodbye, ${name}. We would like to get your feedback about our services. Feel free to reply to this email and let us know what didn't work out.`    
+    }) 
+}
+
+module.exports = {
+    sendWelcomeMail,
+    sendLeavingMail
+}
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+require('./db/mongoose')
+require('dotenv').config()
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+
+const app = express()
+const port = process.env.PORT
+
+app.use(express.json()) // Parses incoming json to an object
+app.use(userRouter)
+app.use(taskRouter)
+
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
